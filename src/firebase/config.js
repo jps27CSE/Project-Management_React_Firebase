ed8d9ef1c0c2ef37c49555e13b140ea5359639d1
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-11-11 23:36:33 +0600

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4M-MqgFV1acSTFL19WuKtdRiG1gr_elA",
+  authDomain: "project-management-react-ce901.firebaseapp.com",
+  projectId: "project-management-react-ce901",
+  storageBucket: "project-management-react-ce901.appspot.com",
+  messagingSenderId: "117409339854",
+  appId: "1:117409339854:web:8324f9cb0e2fcc2616339f",
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init service
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+//timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
