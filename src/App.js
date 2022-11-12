1c3cb708809867dd9060f49335c9e648345d2853
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-11-12 15:36:29 +0600

    routing and pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css'
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Dashboard from "./pages/dashboard/Dashboard";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Create from "./pages/create/Create";
+import Project from "./pages/project/Project";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/project/:id" element={<Project />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
