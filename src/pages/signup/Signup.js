68b60991adb4d81b591fce6264ace75a69ab668c
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-11-26 13:05:36 +0600

    signup form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,5 +1,46 @@
+import { useState } from "react";
 import "./Signup.css";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+
+  return (
+    <form className="auth-form">
+      <h2>Sign Up</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>Display Name:</span>
+        <input
+          required
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>Profile Picture:</span>
+        <input required type="file" />
+      </label>
+    </form>
+  );
 }
